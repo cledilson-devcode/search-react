82f21fb31364dd7fad966ff6639d0e94160e83f6
Cledilson Filho <cledilsondevoffice@gmail.com>
2023-05-24 17:31:01 -0300

    Iniciando projeto com bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container text-center'>
+      <h1 className='py-5 text-uppercase'>Github profile</h1>
+      <form>
+        <div className='form-group'>
+          <label>Github user</label>
+          <div className='input-group'>
+            <input type='text' className='form-control' required />
+            <span className='input-group-btn'>
+              <button type='submit' className='btn btn-success'>
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
