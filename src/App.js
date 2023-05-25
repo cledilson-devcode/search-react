a7c7c96cef02d221b7975ab644669b131c25ac91
Cledilson Filho <cledilsondevoffice@gmail.com>
2023-05-25 10:01:57 -0300

    Feita a integração com a API do github.com/users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import GithubImage from './github-logo.png';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://api.github.com/users/${search}`)
+    .then(response => response.json())
+    .then(userResponse => setUserData(userResponse));
+  }
+
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  
+  console.log(userData);
+
   return (
     <div className='container text-center'>
       <h1 className='py-5 text-uppercase'>Github profile</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='form-group'>
-          <label>Github user</label>
           <div className='input-group'>
-            <input type='text' className='form-control' required />
+            <input type='text' className='form-control' required  value={search} onChange={handleChange} />
             <span className='input-group-btn'>
               <button type='submit' className='btn btn-success'>
                 Search
@@ -18,6 +37,18 @@ function App() {
           </div>
         </div>
       </form>
+      <div className='py-5'>
+        <img src={GithubImage} className='reponsive rounded-circle' alt='' height="200px"/>
+        <h1 className=''>
+          <a href="https://github.com/cledilson-devcode" target="_blank" rel="noopener noreferrer">
+            Cledilson
+          </a>
+        </h1>
+        <h3>São Luís - MA</h3>
+        <p>
+          <a href='https://github.com/cledilson-devcode/search-react' className='text-info'>Projeto Search React</a>
+        </p>
+      </div>
     </div>
   );
 }
